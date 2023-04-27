b3364f4e14df7bf41bfa9984cc6cc700e3a82244
bhwk <120708160+bhwk@users.noreply.github.com>
2023-04-28 03:57:13 +0800

    feat: messages container, chips for channels

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
-	import { messages } from '../lib/store';
-	import { intialiseClient } from '../lib/utils/twitchClient';
+	import { messages, channels } from '../lib/store';
+	import { intialiseClient, joinChannel, leaveChannel } from '../lib/utils/twitchClient';
+	import { scale } from 'svelte/transition';
 	import type { Client } from 'tmi.js';
 
 	let client: Client;
+	let value: string;
 
 	onMount(async () => {
 		client = await intialiseClient();
@@ -18,12 +20,40 @@
 	});
 </script>
 
-<div class="container h-full mx-auto flex-col justify-center items-center">
-	{#each $messages as chat}
-		<div>
-			{chat.channel}
-			{chat.userstate.username}
-			{chat.message}
-		</div>
-	{/each}
+<div class="container h-full mx-auto flex flex-col gap-4 md:p-4 p-2">
+	<div
+		class="h-3/4 w-full variant-filled-surface rounded-lg md:p-4 flex flex-col-reverse overflow-y-auto gap-2 p-2"
+	>
+		{#each $messages as chat}
+			<div>
+				{chat.channel}
+				<span style="color: {chat.userstate.color};">{chat.userstate.username}</span>
+				{chat.message}
+			</div>
+		{/each}
+	</div>
+	<form
+		on:submit|preventDefault={() => {
+			joinChannel(client, value);
+			value = '';
+		}}
+	>
+		<input
+			bind:value
+			class="variant-ringed-primary input"
+			type="text"
+			placeholder="Enter channel to join"
+		/>
+	</form>
+	<div class="flex space-x-2">
+		{#each $channels as channel (channel)}
+			<button
+				transition:scale|local={{ duration: 150, opacity: 0 }}
+				class="chip variant-filled"
+				on:click={() => {
+					leaveChannel(client, channel);
+				}}>{channel}</button
+			>
+		{/each}
+	</div>
 </div>
