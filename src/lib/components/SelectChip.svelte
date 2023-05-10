957eaeb606ca4ad37ae1c5a380f728b66ab4c2ee
bhwk <120708160+bhwk@users.noreply.github.com>
2023-05-11 01:39:46 +0800

    feat: create selected chip

---
+++ src/lib/components/SelectChip.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let channel: string;
+	export let selected = false;
+	export let clickHandle: Function;
+
+	$: chipStateClass = selected ? 'variant-filled' : 'variant-soft';
+</script>
+
+<span
+	class="chip {chipStateClass} "
+	on:click={() => {
+		selected = !selected;
+		clickHandle();
+	}}
+>
+	{channel}
+</span>
