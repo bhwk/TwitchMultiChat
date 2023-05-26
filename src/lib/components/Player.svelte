5f1c7e581cd96a4751a2fc1ed2ebaa82e2746c91
bhwk <120708160+bhwk@users.noreply.github.com>
2023-05-26 15:59:38 +0800

    feat: add quality selector for streams

--- src/lib/components/Player.svelte
+++ src/lib/components/Player.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import Hls from 'hls.js';
 	import { onMount } from 'svelte';
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 	export let channel: string;
 	export let streams: StreamData;
 
 	let video: HTMLMediaElement;
+	let clicked: boolean = false;
+	let selectedQuality: string = 'best';
 
 	interface StreamData {
 		[key: string]: string;
 	}
+
 	async function setVideoSource(stream: string) {
 		if (video.canPlayType('application/vnd.apple.mpegurl')) {
 			video.src = stream;
@@ -17,13 +21,6 @@
 			hls.loadSource(stream);
 			hls.attachMedia(video);
 		}
-
-		video.onloadedmetadata = () => {
-			video.onplay = () => {
-				video.currentTime = video.duration - 8;
-			};
-			console.log(video.currentTime, video.duration);
-		};
 
 		video.play();
 	}
@@ -36,18 +33,28 @@
 {#if streams}
 	<div
 		id={channel}
-		class="flex-grow basis-1/4 flex-shrink group relative border-surface-500 border-2 min-w-0 min-h-0"
+		class="flex-grow basis-1/3 flex-shrink group relative min-w-0 min-h-0 overflow-y-scroll"
 	>
 		<!-- svelte-ignore a11y-media-has-caption -->
-		<video bind:this={video} />
-		<div class="hidden group-hover:block absolute bottom-0 right-0">
-			{#each Object.entries(streams) as [quality, url]}
-				<button
-					on:click={() => {
-						setVideoSource(url);
-					}}>{quality}</button
-				>
-			{/each}
+		<video bind:this={video} class="aspect-video w-full h-full" />
+		<div
+			class="absolute opacity-0 top-0 right-0 invisible group-hover:opacity-100 group-hover:visible flex flex-col transition-opacity ease-in-out duration-100 variant-glass-surface w-[100px] md:w-[150px] max-h-full"
+		>
+			<button on:click={() => (clicked = !clicked)}>Quality</button>
+			{#if clicked}
+				<ListBox active="variant-filled-surface" class="overflow-y-scroll">
+					{#each Object.entries(streams) as [quality, url]}
+						<ListBoxItem
+							active="variant-filled-surface"
+							on:click={() => setVideoSource(url)}
+							bind:group={selectedQuality}
+							value={quality}
+							name={quality}>{quality}</ListBoxItem
+						>
+					{/each}
+				</ListBox>
+			{/if}
+			<div class="flex flex-col-reverse" />
 		</div>
 	</div>
 {/if}
